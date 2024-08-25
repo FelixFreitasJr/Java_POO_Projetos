2dd1b3d04185e4b960f40c78e03a16d9c30571e6
Felix Freitas Jr <118613377+FelixFreitasJr@users.noreply.github.com>
2024-08-25 17:42:11 -0300

    Create main.java

---
+++ Lógica de Programação Algoritmos usando Java (Iniciante)/Estrutura Condicional/Exercício 02/main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        
+
+        if (N % 2 == 0){
+            System.out.println("PAR");
+        } else {
+            System.out.println("IMPAR");
+        }
+
+        sc.close();
+    }
+}
