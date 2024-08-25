34bf27444d93d492d9493a09fdae35a47ebcf23c
Felix Freitas Jr <118613377+FelixFreitasJr@users.noreply.github.com>
2024-08-25 17:07:18 -0300

    Create main.java

---
+++ Lógica de Programação Algoritmos usando Java (Iniciante)/Estrutura sequencial/Exercício 02/main.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double R, A, pi = 3.14159;
+
+            R = sc.nextDouble();
+            A = pi * R * R;
+        
+        System.out.printf("A=%.4f%n", A);
+
+        sc.close();
+    }
+}
