e2337bdcd8e863fb2d7ed0198226179569e0bf64
Felix Freitas Jr <118613377+FelixFreitasJr@users.noreply.github.com>
2024-08-27 11:21:32 -0300

    Create main.java

---
+++ Lógica de Programação Algoritmos usando Java (Iniciante)/Estrutura Repetitiva/Exercício 03/main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int alcool = 0;
+		int gasolina = 0;
+		int diesel = 0;
+		
+		int tipo = sc.nextInt();
+		
+		while (tipo != 4) {
+			if (tipo == 1) {
+				alcool += + 1;
+			}
+			else if (tipo == 2) {
+				gasolina += + 1;
+			}
+			else if (tipo == 3) {
+				diesel += + 1;
+			}
+			
+			tipo = sc.nextInt();
+		}
+
+		System.out.println("MUITO OBRIGADO");
+		System.out.println("Alcool: " + alcool);
+		System.out.println("Gasolina: " + gasolina);
+		System.out.println("Diesel: " + diesel);
+		
+		sc.close();
+	}
+}
