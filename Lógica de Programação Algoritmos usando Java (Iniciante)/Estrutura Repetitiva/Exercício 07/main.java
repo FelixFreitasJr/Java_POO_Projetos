7809ace86eae997a2ee4ec89aacd3b1d9a15be57
Felix Freitas Jr <118613377+FelixFreitasJr@users.noreply.github.com>
2024-09-25 21:13:06 -0300

    Create main.java

---
+++ Lógica de Programação Algoritmos usando Java (Iniciante)/Estrutura Repetitiva/Exercício 07/main.java
@@ -0,0 +1,30 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		
+		for (int i=0; i<n; i++) {
+			
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			
+			if (y == 0) {
+				System.out.println("divisao impossivel");
+			}
+			else {
+				double div = (double) x / y;
+				System.out.printf("%.1f%n", div);
+			}
+		}
+		
+		sc.close();
+	}
+
+}
